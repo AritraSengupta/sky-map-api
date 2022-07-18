b9758eb17a840f63a7aa7dd8be5b4b7f54160ff4
Aritra Sengupta <c-asengupta@maven.io>
2022-07-18 18:52:19 +0530

    first version with example text

--- src/controller/getSkyMap.js
+++ src/controller/getSkyMap.js
@@ -4,22 +4,82 @@ const fs = require('fs');
 var exec = require('child_process').exec;
 
 async function getMetadata(filePath) {
-  await sharp(filePath)
-    .tint({ r: 255, g: 240, b: 16 })
-    .sharpen()
-    .toFile("grayscale.png");
+  try {
+    const width = 1200;
+    const height = width * 1.414;
+    const text = "The night sky";
+
+    const svgBackground = `
+    <svg>
+      <style>
+      .title { fill: #ffffff; font-size: 70px; font-weight: bold;}
+      </style>
+      <g>
+        <rect x="0" y="0" width="${width}" height="${height}" fill="black"></rect>
+        <text x="0" y="${height - 100}" class="title">The Night Sky</text>
+      </g>
+    </svg>
+    `;
+
+    const svgBackgroundBuffer = Buffer.from(svgBackground);
+    const imageBackground = await sharp(svgBackgroundBuffer)
+      .composite([
+        {
+          input: filePath,
+          top: 0,
+          left: 0,
+        },
+      ])
+      .tint({ r: 255, g: 240, b: 16 })
+      .sharpen()
+      .toFile("grayscale.png");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const getSkyMap = async (req, res, next) => {
   console.log(req.query);
-  const date = '2022-07-04+12%3A26%3A10';
-  const jd = '2459765.01817';
-  const lat = '5%B012%27';
-  const lon = '4%B030%27';
+  const {
+    date,
+    jd,
+    ns,
+    lat,
+    lon,
+    ew,
+  } = req.query;
+  // const date = '2022-07-04+12%3A26%3A10';
+  // const jd = '2459765.01817';
+  // const lat = '5%B012%27';
+  // const lon = '4%B030%27';
   const baseUrl = 'https://www.fourmilab.ch';
 
   // get some posts
-  let result = await axios.get(`${baseUrl}/cgi-bin/Yoursky?date=1&utc=${date}&jd=${jd}&lat=${lat}&ns=North&lon=${lon}&ew=West&deepm=2.5&consto=on&limag=5.5&starnm=2.0&starbm=2.5&imgsize=1024&fontscale=1.0&scheme=2&elements=`);
+  let result = await axios.get(`${
+    baseUrl
+  }/cgi-bin/Yoursky?date=1
+  &utc=${
+    date
+  }&jd=${
+    jd
+  }&lat=${
+    lat
+  }&ns=${
+    ns
+  }&lon=${
+    lon
+  }&ew=${
+    ew
+  }
+  &deepm=2.5
+  &consto=on
+  &limag=5.5
+  &starnm=2.0
+  &starbm=2.5
+  &imgsize=1024
+  &fontscale=1.0
+  &scheme=2
+  &elements=`);
   const extract = result.data
   const match = extract.match(/"\/cgi-bin\/Yoursky\?di=.*" usemap/);
   const removeFirst = match[0].replace('\"', '');
